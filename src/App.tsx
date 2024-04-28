c2095cea31aafed2a118b63063dc0bcc2cf05e8f
bhumika1312 <bhumikabhatt1312@gmail.com>
2024-04-28 12:38:13 +0530

    Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import {setInternal} from "timers";
  */
 interface IState {
   data: ServerRespond[],
-  showGraph: boolean,
 }
 
 /**
@@ -24,7 +23,6 @@ class App extends Component<{}, IState> {
       // data saves the server responds.
       // We use this state to parse data down to the child element (Graph) as element property
       data: [],
-      showGraph: false,
     };
   }
 
@@ -40,21 +38,6 @@ class App extends Component<{}, IState> {
   /**
    * Get new data from server and update the state with the new data
    */
-  getDataFromServer() {
-      let x = 0;
-      const interval = setInternal(() => {
-        DataStreamer.getData((serverResponds: ServerRespond[]) => {
-           this.setState({
-               data: serverResponds,
-               showGraph: true,
-           });
-        });
-        x++;
-        if (x > 1000) {
-            clearInterval(interval)
-        }
-      }, 100);
-  }
 
 
   /**
